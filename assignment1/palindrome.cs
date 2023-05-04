0d4f6b5846b7d7618e9a69640fa35d12054d21b8
jnvThesandeep <122285479+jnvThesandeep@users.noreply.github.com>
2023-05-04 17:05:58 +0530

    Add files via upload

---
+++ assignment1/palindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyPalindrome{
+
+public class palindrome
+{
+
+
+    public static void Main(string[] args)
+    {
+
+        string str = Console.ReadLine().ToLower();
+        int flag = 1;
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - 1 - i])
+            {
+                flag = 0;
+            }
+            else
+            {
+                flag = 1;
+            }
+        }
+        if (flag == 1)
+        {
+            Console.WriteLine("It is palindrome");
+        }
+        else if (flag == 0)
+        {
+            Console.WriteLine("It is not palindrome");
+        }
+    }
+}
+
+}
---
+++ assignment1/static.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StativVariable{
+
+class StaticVar{
+
+public static string Welcome="Welcome to invia Learning center";
+
+}
+
+class programName{
+
+    static void Main(string[] args){
+
+        Console.WriteLine(StaticVar.Welcome);
+    }
+}
+
+}
